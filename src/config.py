b1293666c52acff7bb5f6f8d2a225fd293ac56f1
arkarishi <arkaprovo.rishi@gmail.com>
2024-06-29 11:10:32 +0530

    changes

---
+++ src/config.py
@@ -0,0 +1,7 @@
+DATA_PATH='C:/Users/ARUP SARKAR/Desktop/ML/Linear Reg Practical Imp/smartwatchprice/data/smartwatches.csv'
+EXTRA_COL='Unnamed: 0'
+IMP_COL_NUMERICAL=['Current Price','Original Price',
+                   'Rating','Number OF Ratings','Display Size']
+IMP_COL_CATEGORICAL=['Brand','Model Name','Dial Shape','Strap Material']
+CLEAN_FILE_PATH='C:/Users/ARUP SARKAR/Desktop/ML/Linear Reg Practical Imp/smartwatchprice/data/processed_smartwatches.csv'
+CLEAN_FILE_PATH='C:/Users/ARUP SARKAR/Desktop/ML/Linear Reg Practical Imp/smartwatchprice/src/Models/model.pkl'
